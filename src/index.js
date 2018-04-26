86ee6c8d1d14c563259052de2c50a03aee20e02b
Chris Atkin <atkinchris@gmail.com>
2018-04-26 09:06:18 +0100

    Test arrays

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import { hello_world, wasmBooted } from './lib.rs'
+import { get_array, wasmBooted } from './lib.rs'
 
 const output = document.getElementById('output')
 
-const print = () => {
-  const message = hello_world('Chris', 'A')
-
-  output.innerText = message
-}
+const run = () => get_array()
+const print = (result) => { output.innerText = JSON.stringify(result) }
 
 wasmBooted
+  .then(run)
+  .catch(e => e.message)
   .then(print)
-  .catch(console.error)
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@ extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-#[no_mangle]
 pub extern fn hello_world(mut first_name: &str, mut last_name: &str) -> String {
     // This is fairly silly code but it is just an example...
     if first_name.is_empty() {
@@ -15,4 +14,16 @@ pub extern fn hello_world(mut first_name: &str, mut last_name: &str) -> String {
         last_name = "Doe";
     }
     format!("Hello, {} {}!", first_name, last_name)
+}
+
+#[wasm_bindgen]
+pub fn get_array() -> Vec<JsValue> {
+    vec![
+        JsValue::from(1),
+        JsValue::from(2),
+        JsValue::from(3),
+        JsValue::from(4),
+        JsValue::from(5),
+        JsValue::from_str("Chris"),
+    ]
 }
